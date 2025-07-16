8d79d47129f8167e3b49067ae30b99b602fbc008
Kolluri Manju <manju.kolluri@achalasolutions.com>
2025-07-16 14:10:35 +0530

    Code pipeline automatic trigger

--- cloud_infra/iam_roles.tf
+++ cloud_infra/iam_roles.tf
@@ -1,8 +1,3 @@
-# resource "aws_iam_role" "lambda_exec" {
-#   name = "lambda_exec"
-#   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
-# }
-
 resource "aws_iam_role" "lambda_exec" {
   name = "lambda_exec_role"
   assume_role_policy = jsonencode({
